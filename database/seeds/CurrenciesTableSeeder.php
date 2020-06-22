065ca89463b4313477d1b3f3b2cbafce4c6ab524
Sebastian Bolivar Tapias <sebastian.bolivart@udea.edu.co>
2020-06-22 12:55:52 -0500

    Initial commit

---
+++ database/seeds/CurrenciesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+use App\Currency;
+use Illuminate\Database\Seeder;
+
+class CurrenciesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currencies = [
+            'usd',
+            'eur',
+            'gbp',
+        ];
+
+        foreach($currencies as $currency){
+            Currency::create([
+                'iso' => $currency,
+            ]);
+        }
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Make a payment</div>
+
+                <div class="card-body">
+                    <form action="#" method="POST" id="paymentForm">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label for="">How much you want to pay?</label>
+                                <input class="form-control" type="number" name="value" min="5" step="0.01" value="{{mt_rand(500,100000) / 100}}">
+                                <small class="form-text text-muted">
+                                    Use values with up to two decimal positions, using dot "."
+                                </small>
+                            </div>
+                        </div>
+                        <div class="text-center mt-3">
+                            <button type="submit" id="payButton" class="btn btn-primary btn-lg">Pay</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
