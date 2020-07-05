dd25a252ccea4996c0f56ca17bca733690233700
Sebastian Bolivar Tapias <sebastian.bolivart@udea.edu.co>
2020-07-05 12:32:56 -0500

    Commit 4
    se inicia el controlador de pagos pero hay un error con la validacion.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Currency;
+use App\PaymentPlatform;
 
 class HomeController extends Controller
 {
@@ -25,9 +26,11 @@ class HomeController extends Controller
     public function index()
     {
         $currencies = Currency::all();
+        $paymentPlatforms = PaymentPlatform::all();
 
         return view('home')->with([
             'currencies' => $currencies,
+            'paymentPlatforms' => $paymentPlatforms,
         ]);
     }
 }
---
+++ resources/views/components/paypal-collapse.blade.php
@@ -0,0 +1,1 @@
+<small>You willbe redirected to the PayPal checkout page</small>
