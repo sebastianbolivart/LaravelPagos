f3254ddd2c906660d09449ea2284f3b44b6e0866
sebastianbolivart <sebastian.bolivart@udea.edu.co>
2020-08-02 11:37:02 -0500

    Se corrige el error que no permitia el buen funcionamiento del form principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,19 +10,30 @@
                 <div class="card-body">
                     <form action="{{ route('pay') }}" method="POST" id="paymentForm">
                         @csrf
+
                         <div class="row">
                             <div class="col-auto">
-                                <label for="">How much you want to pay?</label>
-                                <input required class="form-control" type="number" name="value" min="5" step="0.01" value="{{mt_rand(500,100000) / 100}}">
+                                <label>How much you want to pay?</label>
+                                <input
+                                    type="number"
+                                    min="5"
+                                    step="0.01"
+                                    class="form-control"
+                                    name="value"
+                                    value="{{ mt_rand(500, 100000) / 100 }}"
+                                    required
+                                >
                                 <small class="form-text text-muted">
                                     Use values with up to two decimal positions, using dot "."
                                 </small>
                             </div>
                             <div class="col-auto">
                                 <label>Currency</label>
-                                <select name="currency" id="" class="custom-select" required>
-                                    @foreach ($currencies as $currency )
-                                <option value="{{$currency->iso}}">{{strtoupper($currency->iso)}}</option>
+                                <select class="custom-select" name="currency" required>
+                                    @foreach ($currencies as $currency)
+                                        <option value="{{ $currency->iso }}">
+                                            {{ strtoupper($currency->iso) }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
@@ -30,21 +41,34 @@
                         <div class="row mt-3">
                             <div class="col">
                                 <label>Select the desired payment platform:</label>
-                            <div class="form-group" id="toggler">
-                                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                <div class="form-group" id="toggler">
+                                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                        @foreach ($paymentPlatforms as $paymentPlatform)
+                                            <label
+                                                class="btn btn-outline-secondary rounded m-2 p-1"
+                                                data-target="#{{ $paymentPlatform->name }}Collapse"
+                                                data-toggle="collapse"
+                                            >
+                                                <input
+                                                    type="radio"
+                                                    name="payment_platform"
+                                                    value="{{ $paymentPlatform->id }}"
+                                                    required
+                                                >
+                                                <img class="img-thumbnail" src="{{ asset($paymentPlatform->image) }}">
+                                            </label>
+                                        @endforeach
+                                    </div>
                                     @foreach ($paymentPlatforms as $paymentPlatform)
-                                            <label class="btn btn-outline-secondary rounded m-2 p-1" data-target="#{{ $paymentPlatform->name }}Collapse" data-toggle="collapse">
-                                            <input type="radio" name="payment_platform" id="{{$paymentPlatform->id}}" required>
-                                            <img class="img-thumbnail" src="{{asset($paymentPlatform->image)}}">
-                                        </label>
+                                        <div
+                                            id="{{ $paymentPlatform->name }}Collapse"
+                                            class="collapse"
+                                            data-parent="#toggler"
+                                        >
+                                            @includeIf('components.' . strtolower($paymentPlatform->name) . '-collapse')
+                                        </div>
                                     @endforeach
                                 </div>
-                                @foreach ($paymentPlatforms as $paymentPlatform)
-                                    <div id="{{ $paymentPlatform->name }}Collapse" class="collapse" data-parent="#toggler">
-                                        @includeIf('components.' . strtolower($paymentPlatform->name) . '-collapse')
-                                    </div>
-                                @endforeach
-                            </div>
                             </div>
                         </div>
                         <div class="text-center mt-3">
